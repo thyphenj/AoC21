ee4807995991b326cc33fe3b6ee49e397b1b4aac
t-j <t-j@terence-john.co.uk>
2021-12-01 12:10:34 +0000

    Day1 completed

--- AoC21/Program.cs
+++ AoC21/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AoC21
 {
@@ -6,7 +7,35 @@ namespace AoC21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = File.ReadAllLines("input.txt");
+
+            int inc = 0;
+            int i = 0;
+            int cur = int.Parse(input[i++]);
+            while (i < input.Length)
+            {
+                if (int.Parse(input[i - 1]) < int.Parse(input[i]))
+                    inc++;
+                i++;
+            }
+            Console.WriteLine(inc);
+
+            inc = 0;
+            i = 0;
+            cur = int.Parse(input[i]) + int.Parse(input[i + 1]) + int.Parse(input[i + 2]);
+            int nxt = int.Parse(input[i + 1]) + int.Parse(input[i + 2]) + int.Parse(input[i + 3]);
+            while (i + 3 < input.Length)
+            {
+                if (cur < nxt)
+                    inc++;
+                i++;
+                cur = nxt;
+                if (i + 3 < input.Length)
+                    nxt = int.Parse(input[i + 1]) + int.Parse(input[i + 2]) + int.Parse(input[i + 3]);
+            }
+            Console.WriteLine(inc);
+
+
         }
     }
 }
