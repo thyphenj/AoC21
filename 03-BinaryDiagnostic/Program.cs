9a861f33ac7edc9326fd5955f33d562f44785a65
t-j <t-j@terence-john.co.uk>
2021-12-03 11:34:08 +0000

    Oh dear

--- 03-BinaryDiagnostic/Program.cs
+++ 03-BinaryDiagnostic/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace _03_BinaryDiagnostic
 {
@@ -7,33 +8,74 @@ namespace _03_BinaryDiagnostic
     {
         static void Main(string[] args)
         {
-            //string[] input = { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010" };
-            string[] input = File.ReadAllLines("input.txt");
+            string[] originalFile = { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010" };
+            //string[] originalFile = File.ReadAllLines("input.txt");
 
-            long[] ones = new long[input[0].Length];
-            long[] zros = new long[input[0].Length];
-
-            for (int i = 0; i < input.Length; i++)
+            List<Number> input = new List<Number>();
+            foreach (var str in originalFile)
             {
-                for (int j = 0; j < input[0].Length; j++)
+                input.Add(new Number(str));
+            }
+
+            int wordCount = input.Count;
+            int wordLength = input[0].ValueAsString.Length;
+            //-----------------------------------------------------------
+            // -- Part 1 : Find Gamma and Delta
+
+            long[] ones = new long[wordLength];
+
+            //-- Count "set" bits
+            foreach (var currWord in input)
+            {
+                for (int j = 0; j < wordLength; j++)
                 {
-                    if (input[i][j] == '0')
-                        zros[j]++;
-                    else
+                    if (currWord.ValueAsString[j] == '1')
                         ones[j]++;
                 }
             }
-            long gamma = 0;
-            long delta = 0;
-            for (int i = 0; i < input[0].Length; i++)
+
+            //-- Convert array to string?!
+            long gamma = 0, delta = 0;
+            string gammaString = "", deltaString = "";
+
+            for (int i = 0; i < wordLength; i++)
             {
-                int digit = (ones[i] > zros[i]) ? 1 : 0;
-                    
-                gamma = gamma * 2 + digit;
-                delta = delta * 2 + (digit + 1) % 2;
+                gamma *= 2;
+                delta *= 2;
+                if (ones[i] >= wordCount / 2)
+                {
+                    gamma++;
+                    gammaString += '1';
+                    deltaString += '0';
+                }
+                else
+                {
+                    delta++;
+                    gammaString += '0';
+                    deltaString += '1';
+                }
             }
 
-            Console.WriteLine($"{gamma} {delta} -> {gamma*delta}");
+            Console.WriteLine($"Part 1 : {gamma} {delta} -> {gamma * delta}");
+
+            //-----------------------------------------------------------
+            // -- Part 1 : Find Gamma and Delta
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                foreach (var num in input)
+                {
+                    if (!num.Removed)
+                    {
+                        if (num.ValueAsString[i] != gammaString[i])
+                        {
+                            num.Removed = true;
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
         }
+
     }
 }
